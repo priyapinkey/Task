80d479cf2e8cd3e99fbf85fac74948f75f5100b3
priyapinkey <priyapinkey7588@gmail.com>
2024-09-13 11:27:25 +0530

    new code added

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,19 @@
+package org.add;
+
+public class GreensTech {
+      private void companyName() {
+		System.out.println("Company name is TCS");
+	}
+      private void companyId() {
+		System.out.println("Company id is 12345");
+	}
+      private void companyAddress() {
+		System.out.println("No 20 pillayar koil street chennai-60007");
+	}
+      public static void main(String[] args) {
+		GreensTech g= new GreensTech();
+	    g.companyName();
+		g.companyId();
+		g.companyAddress();
+	}
+}
